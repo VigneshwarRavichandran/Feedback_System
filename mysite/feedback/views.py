8d4f35a0e49aa77f734c393fa1d834f7d9ac3fc9
Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>
2019-11-05 13:16:11 +0530

    Added Votes and Comments feature
    Signed-off-by: Vigneshwar Ravichandran <vigneshwarravichandran@gmail.com>

--- mysite/feedback/views.py
+++ mysite/feedback/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from .models import Post, Vote, Comment
 from django.views import View
 from django.contrib import messages
+from django.db.models import Count
 from django.http import HttpResponse
 
 def register(request):
@@ -46,23 +47,21 @@ class PostView(View):
 		context = {
 		 'posts' : []
 		}
-		posts = Post.objects.all()
-		print(posts)
-		# posts = Post.objects.prefetch_related('votes', 'comments').annotate(Count('votes', distinct=True)).annotate(Count('comments', distinct=True)).values(
-		# 	'id', 'title', 'content', 'votes__count', 'comments__count')
-		# voted_post_ids = Vote.objects.prefetch_related('posts').filter(votedby_id=request.session['user_id']).values_list('post', flat=True)
-		# for post in posts:
-		# 	is_voted = False
-		# 	if post['id'] in voted_post_ids:
-		# 		is_voted = True
-		# 	context['posts'].append({
-		# 		'id' : post['id'],
-		# 		'title' : post['title'],
-		# 		'content' : post['content'],
-		# 		'votes' : post['votes__count'],
-		# 		'is_voted' : is_voted,
-		# 		'total_comments' : post['comments__count']
-		# 	})
+		posts = Post.objects.prefetch_related('vote', 'comment').annotate(Count('vote', distinct=True)).annotate(Count('comment', distinct=True)).values(
+			'id', 'title', 'content', 'vote__count', 'comment__count')
+		voted_post_ids = Vote.objects.prefetch_related('posts').filter(votedby_id=request.session['user_id']).values_list('post', flat=True)
+		for post in posts:
+			is_voted = False
+			if post['id'] in voted_post_ids:
+				is_voted = True
+			context['posts'].append({
+				'id' : post['id'],
+				'title' : post['title'],
+				'content' : post['content'],
+				'votes' : post['vote__count'],
+				'is_voted' : is_voted,
+				'total_comments' : post['comment__count']
+			})
 		return render(request, self.template_name, context)
 
 	def post(self, request, *args, **kwargs):
@@ -73,3 +72,58 @@ class PostView(View):
 		post.save()
 		messages.success(request, 'Posted Successfully')
 		return redirect('posts')
+
+def create_vote(request, post_id):
+	user_id = request.session['user_id']
+	vote, created = Vote.objects.get_or_create(votedby_id=user_id, post_id=post_id)
+	if created:
+		messages.success(request, 'Voted Successfully')
+	else:
+		messages.warning(request, "Sorry, you have voted already!")
+	return redirect('posts')
+
+def get_post(request, post_id):
+	user_id = request.session['user_id']
+	post = Post.objects.prefetch_related('vote', 'comment').filter(id=post_id).annotate(Count('vote', distinct=True)).annotate(Count('comment', distinct=True)).values(
+			'id', 'title', 'content', 'vote__count', 'comment__count')
+	voted_post_ids = Vote.objects.prefetch_related('posts').filter(votedby_id=request.session['user_id']).values_list('post', flat=True)
+	post_comments = Post.objects.prefetch_related('comment').filter(id=post_id).values(
+			'comment__commentedby__username','comment__text')
+	post = post[0]
+	comments = []
+	for post_comment in post_comments:
+		if post_comment['comment__commentedby__username'] is not None:
+			comments.append({
+					'commentedby' : post_comment['comment__commentedby__username'],
+					'text' : post_comment['comment__text']
+				})
+	is_voted = False
+	if post['id'] in voted_post_ids:
+		is_voted = True
+	context = {
+		'id' : post['id'],
+		'title' : post['title'],
+		'content' : post['content'],
+		'votes' : post['vote__count'],
+		'is_voted' : is_voted,
+		'total_comments' : post['comment__count'],
+		'comments' : comments
+	}
+	return render(request, 'post/post.html', context)
+
+
+def create_comment(request, post_id):
+	context = {
+		'posts' : None
+	}
+	user_id = request.session['user_id']
+	if request.method == 'POST':
+		if 'comment' in request.POST:
+			post_comment = request.POST.get('post_comment')
+			comment = Comment.objects.create(commentedby_id=user_id, text=post_comment, post_id=post_id)
+			messages.success(request, 'Commented Successfully')
+	return redirect('get_post', post_id)
+
+def logout_view(request):
+	logout(request)
+	return redirect(login)
